647c441c54b786946bcb652b8494bf65756833a8
setiadjie <102172311+setiadjie@users.noreply.github.com>
2022-04-20 13:43:45 +0700

    Add files via upload

---
+++ while.py
@@ -0,0 +1,15 @@
+# while loop
+
+# while kondisi:
+# 	aksi ini
+# 	aksi itu
+
+# akhir dari program
+
+angka = 0
+print(f"Angka Sekarang -> {angka} ")
+
+while angka < 5:
+	angka += 1
+	print(f"Angka Sekarang -> {angka}\n")
+	print("Nama Saya Setiadjie")
